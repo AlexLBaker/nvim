bf9fb1ee166df30156ca4ed2699ad8ea85b23c9d
albnewpal3@gmail.com <albnewpal3@gmail.com>
2026-02-04 15:34:23 -0500

    fix: snacks.lua fix, unicode chars and missing brace.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,18 +1,26 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    picker = {
-      -- This targets the actual search logic
-      sources = {
-        files = {
-          -- hidden = true, -- show hidden files (.dotfiles)
-          ignored = false, -- keep this false to respect .gitignore
-          exclude = {
-            ".git", ".venv", "venv", "build", "dist", "__pycache__",
-            "*.lock", "*.pyc", "*.pyo", "*.xlsx", "__init__.py"
-          },
-        },
-      },
-    },
-  },
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+          ignored = false, -- keep this false to respect .gitignore
+          exclude = {
+            ".git",
+            ".venv",
+            "venv",
+            "build",
+            "dist",
+            "__pycache__",
+            "*.lock",
+            "*.pyc",
+            "*.pyo",
+            "*.xlsx",
+            "__init__.py",
+          },
+        },
+      },
+    },
+  },
 }
