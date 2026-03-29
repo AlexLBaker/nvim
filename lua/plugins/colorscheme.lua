705851f05a777f700e23a486eb12944e904fc832
albnewpal3@gmail.com <albnewpal3@gmail.com>
2026-03-29 08:12:33 -0400

    feat: Updated colorscheme to a darker theme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,19 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "rebelot/kanagawa.nvim",
+    lazy = false,
     priority = 1000,
     opts = {
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      transparent_background = false,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-        },
-        telescope = true,
-        which_key = true,
+      theme = "dragon",
+      background = {
+        dark = "dragon",
       },
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
